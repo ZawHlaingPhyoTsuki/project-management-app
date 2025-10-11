762212f2dfed73efd53d81fdcb5da6b31fa91a3e
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2025-10-11 09:57:33 +0700

    added: Dark Theme

---
+++ src/components/providers/index.tsx
@@ -0,0 +1,15 @@
+import type * as React from "react";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
